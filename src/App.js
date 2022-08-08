2ef2bfcf0e611c13946e5942d629c625fb4a1038
FirasMyacoup <firas.yacoup1@gmail.com>
2022-08-08 23:41:02 +0300

    lab15

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Login from './Login';
+import Logout from './Logout';
+import Profile from './User';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <Logout/>
+      <Profile/>
+      
+    </div>
+  );
+}
+
+export default App;
